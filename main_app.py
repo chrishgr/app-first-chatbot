96755174a826adbe87f9c11ba9f7f84f71543a8a
chrishgr <138783543+chrishgr@users.noreply.github.com>
2025-10-20 15:07:37 +0200

    Update title and author attribution in main_app.py

--- main_app.py
+++ main_app.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 st.set_page_config(page_title= "title", page_icon= "asdf", layout = "centered")
 
-st.title("Ethics of healthcare chatbot")
+st.title("Chatbot")
 st.subheader(" explanation")
 
 st.markdown("**Welcome to this page** ")
 
 st.divider()
 
-st.markdown("Made by Christian H. Grusd, based entirely on the oreilly course Introduction to Chatbots - Build your first AI assistant")
+st.markdown("Based entirely on the oreilly course Introduction to Chatbots - Build your first AI assistant by ThinkRook")
